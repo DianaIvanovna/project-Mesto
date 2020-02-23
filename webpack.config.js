b772a232b0efb7eed9ec43f9c9653c2e97d27c05
Diana Garbuz <GDI.1999@yandex.ru>
2020-02-24 01:07:30 +0300

    .

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,12 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // добавили плагин
+const webpack = require('webpack');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+// подключаем плагин
+const isDev = process.env.NODE_ENV === 'development';
+// создаем переменную для development-сборки
+
 module.exports = {
   entry: { main: './src/index.js' },
   output: {
@@ -18,8 +24,12 @@ module.exports = {
         }
       },
       {
-        test: /\.css$/,
-        use:  [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'] // добавили минификацию CSS
+        test: /\.css$/i,
+        use: [
+                        (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
+                        'css-loader', 
+                        'postcss-loader'
+                ]
       },
       {
         test: /\.(png|jpg|gif|ico|svg)$/,
@@ -27,21 +37,39 @@ module.exports = {
                 'file-loader?name=../images/[name].[ext]', // указали папку, куда складывать изображения
                 {
                         loader: 'image-webpack-loader',
-                        options: {}
+                        options: {
+                          bypassOnDebug: true,
+                          disable: true,
+                        }
                 },
         ]
- }
+    },
+    {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        loader: 'file-loader?name=./vendor/[name].[ext]'
+    }
     ]
   },
   plugins: [
     new MiniCssExtractPlugin({ // 
-      filename: 'style.[contenthash].css',
+      filename: '[name].[contenthash].css'
     }),
+    new OptimizeCssAssetsPlugin({
+        assetNameRegExp: /\.css$/g,
+        cssProcessor: require('cssnano'),
+        cssProcessorPluginOptions: {
+                preset: ['default'],
+        },
+        canPrint: true
+    }) ,
     new HtmlWebpackPlugin({
       inject: false,
       template: './src/index.html',
       filename: 'index.html'
     }),
-    new WebpackMd5Hash()
+    new WebpackMd5Hash(),
+    new webpack.DefinePlugin({
+      'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+  })
   ]
 };
